39c946e47b6023656e151c0e63c9e2a22a57994b
Kieran Black <kieranlblack@outlook.com>
2020-02-16 14:42:53 -0500

    changed how the data is written to the sd card to save memory

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <Adafruit_BMP280.h>
 #include <Adafruit_MPRLS.h>
 #include <AltSoftSerial.h>
-#include <SDFat.h> // #include <SD.h>
+#include <SDFat.h>
 #include <SPI.h>
 #include <TinyGPS++.h>
 
@@ -10,7 +10,9 @@
 static const bool BUZZER = true;
 
 // Names for the log file
-static const char LOGFILE[10] = "0216L.LOG";
+static const char LOGFILE[10] = "16L.LOG";
+
+static const char* DELIMITER = ",";
 
 static const uint8_t PINCS = 8; // Pin for the SD Card
 static const uint8_t PINTEMP_IN = A1; // Pins for the LM60 temperature sensors
@@ -19,7 +21,6 @@ static const uint8_t PINBUZZ = 3; // Pin for the buzzer
 
 static SdFat sd;
 static SdFile activeFile;
-// static File activeFile;
 
 static Adafruit_BMP280 bmp = Adafruit_BMP280();
 static Adafruit_MPRLS mpr = Adafruit_MPRLS(-1, -1);
@@ -43,10 +44,10 @@ static float speed;
 static uint32_t gpsTime;
 static uint8_t satCount;
 
-// The file buffer
-static char* outBuff;
-
 static uint32_t time; // Holds the time since the program started
+
+// The file buffer
+// static char* outBuff;
 
 static auto buzzTime = [&]() { return BUZZER && (pressure_BMP > 90000 || pressure_MPRLS > 900); };
 
@@ -70,8 +71,6 @@ void setup() {
 
     // pinMode(PINCS, OUTPUT); // Set pinCS as an output pin (SD card data output)
     pinMode(PINBUZZ, OUTPUT); // Set up the buzzer pin as an output
-
-    if (!sd.begin(PINCS, SPI_HALF_SPEED)) sd.initErrorHalt();
 #if DEBUG
     // Check if the BMP sensor is connected
     if (bmp.begin()) {
@@ -87,22 +86,6 @@ void setup() {
         Serial.println(F("Failed to find the MPRLS sensor."));
     }
 
-    // Check if the GPS is connected
-    if (ss.available() > 0) {
-        Serial.println(F("GPS found."));
-    } else {
-        Serial.println(F("GPS not found."));
-    }
-
-    // SD card initialization
-    /* if (SD.begin()) {
-        Serial.println(F("SD card is ready to use."));
-    } else {
-        Serial.println(F("SD card initialization failed."));
-
-        return;
-    } */
-
     if (sd.begin(PINCS, SPI_HALF_SPEED)) {
         Serial.println(F("SD card is ready to use."));
     } else {
@@ -114,12 +97,10 @@ void setup() {
     // Initialise everything
     bmp.begin();
     mpr.begin();
-    // SD.begin();
+    if (!sd.begin(PINCS, SPI_HALF_SPEED)) sd.initErrorHalt();
 #endif
-    // activeFile = SD.open(SENSFILE, FILE_WRITE); // Open the file or create it if it does not exist
-
-    // Check if the sensor file opened okay
-    if (activeFile.open(LOGFILE, O_RDWR | O_CREAT | O_AT_END)) { // activeFile
+    // Try and open the fild to write to it
+    if (activeFile.open(LOGFILE, O_RDWR | O_CREAT | O_AT_END)) {
         activeFile.println(F("T,GT,PBMP,PMPRLS,TBMP,TIN,TEXT,LAT,LNG,ALT,SPD,CNT")); // Write to the file
         activeFile.close(); // Close the file
     } else {
@@ -152,7 +133,7 @@ void loop() {
     if (gps.satellites.isValid()) gps.satellites.value();
     if (gps.time.isValid()) gpsTime = gps.time.value();
 
-    sprintf(outBuff, "%lu,%lu,%.2f,%.2f,%.2f,%.2f,%.2f,%f,%f,%.2f,%.2f,%d", time,
+    /* sprintf(outBuff, "%lu,%lu,%.2f,%.2f,%.2f,%.2f,%.2f,%f,%f,%.2f,%.2f,%d", time,
                                                                         gpsTime,
                                                                         (double) pressure_BMP,
                                                                         (double) pressure_MPRLS,
@@ -163,18 +144,45 @@ void loop() {
                                                                         longitude,
                                                                         (double) alt,
                                                                         (double) speed,
-                                                                        satCount);
+                                                                        satCount); */
+
+    
 
     if (buzzTime()) tone(PINBUZZ, 1500, 1000);
 #if DEBUG
-    Serial.println(sensorOutBuff);
-    Serial.println(gpsOutBuff);
+    Serial.println(outBuff);
     Serial.println();
 #endif
     // We write the data to the SD card
-    // activeFile = SD.open(LOGFILE, FILE_WRITE);
     activeFile.open(LOGFILE, O_RDWR | O_CREAT | O_AT_END);
-    activeFile.println(outBuff);
+
+    // Log all the data to the file, yeah it is ugly but it saves memory
+    // activeFile.println(outBuff);
+    activeFile.print(time);
+    activeFile.print(DELIMITER);
+    activeFile.print(gpsTime);
+    activeFile.print(DELIMITER);
+    activeFile.print(pressure_BMP);
+    activeFile.print(DELIMITER);
+    activeFile.print(pressure_MPRLS);
+    activeFile.print(DELIMITER);
+    activeFile.print(temp_BMP);
+    activeFile.print(DELIMITER);
+    activeFile.print(tempin);
+    activeFile.print(DELIMITER);
+    activeFile.print(tempext);
+    activeFile.print(DELIMITER);
+    activeFile.print(latitude);
+    activeFile.print(DELIMITER);
+    activeFile.print(longitude);
+    activeFile.print(DELIMITER);
+    activeFile.print(alt);
+    activeFile.print(DELIMITER);
+    activeFile.print(speed);
+    activeFile.print(DELIMITER);
+    activeFile.print(satCount);
+    activeFile.println();
+
     activeFile.close();
 
     smartDelay(1000);

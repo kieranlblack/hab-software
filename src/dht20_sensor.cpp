a0f63c8d1e3195fc92702159c3ab13309e4edcdb
Kieran Black <kieranlblack@gmail.com>
2022-04-12 00:34:31 -0400

    add better dht20 debug logging

--- src/dht20_sensor.cpp
+++ src/dht20_sensor.cpp
@@ -17,14 +17,34 @@ bool setup_dht20() {
         success = DHT.begin();
         if (!success) {
             #ifdef DEBUG_DH20
-                DEBUG_STREAM.print(F("unable to setup dht20, "));
+                // copied from the dht20 library example
+                DEBUG_STREAM.print(F("unable to setup dht20, issue is "));
+                int status = DHT.read();
+                switch (status) {
+                case DHT20_OK:
+                    DEBUG_STREAM.println(F("no issue?"));
+                    break;
+                case DHT20_ERROR_CHECKSUM:
+                    DEBUG_STREAM.print(F("checksum"));
+                    break;
+                case DHT20_ERROR_CONNECT:
+                    DEBUG_STREAM.print(F("connection"));
+                    break;
+                case DHT20_MISSING_BYTES:
+                    DEBUG_STREAM.print(F("bytes missing"));
+                    break;
+                default:
+                    DEBUG_STREAM.print(F("unknown"));
+                    break;
+                }
+                DEBUG_STREAM.print(F(" with "));
                 DEBUG_STREAM.print(attempts_remaining - 1);
                 DEBUG_STREAM.println(F(" attempts remaining"));
             #endif
             delay(500);
         } else {
             #ifdef DEBUG_DH20
-                DEBUG_STREAM.print(F("dht20 set up correctly"));
+                DEBUG_STREAM.println(F("dht20 set up correctly"));
             #endif
         }
         attempts_remaining--;
